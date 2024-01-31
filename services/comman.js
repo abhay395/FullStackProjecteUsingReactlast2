eabfccd2c4afd5480fb2e54c3f5415a75fe11021
Abhay prajapati <abhayparja90@gmail.com>
2024-01-31 13:49:46 +0530

    set address

--- services/comman.js
+++ services/comman.js
@@ -9,7 +9,7 @@ exports.senitizeUser = (user)=>{
 }
 exports.cookieExtractor = function(req) {
     if (req && req.cookies) {
-    //    return token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YjViN2NiMGQwMzA4ZGI0NjhlYWI1OCIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzA2NTEzNTYzfQ.CAYQgNPkSopvi5qW4QH4yNF1eH9IUNEqpwoXu5R-K08";
-       return token = req.cookies['jwt'];
+       return token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YjllZWY5ZGY2YzBjMDdkNWIxOTA1MyIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzA2Njg0MTUzfQ.FTGwYvZVzAQJsGtOcpGdymw5gn5mwxgtwcZJSMTkplo";
+    //    return token = req.cookies['jwt'];
     }
 };
